04e0300f53f953a4c404086e9d775b6501ea0882
markettv <168855912+markettv@users.noreply.github.com>
2024-05-19 14:54:36 +0100

    Update oscam_all.sh

--- oscam_all.sh
+++ oscam_all.sh
@@ -1,11 +1,11 @@
-echo "################ oDream_FHD_Mod_v1.1_all #################"
-echo "################ egami image #################"
-echo "############ uploaded by karim #################"
+echo "################ OSCam 11.784- emu-r801 #################"
+echo "################ ALL Images mips - arm #################"
+echo "############ Thanks: audi06_19 #################"
 #!/bin/sh
 #
-wget -O /tmp/enigma2-plugin-extensions-oDream_FHD_Mod_v1.1_all.ipk "https://github.com/markettv/plug/raw/main/enigma2-plugin-extensions-oDream_FHD_Mod_v1.1_all.ipk"
+wget -O /tmp/enigma2-softcams-oscam-all-images_11784-emu-801-arm+mips_all.ipk "https://github.com/markettv/soft/raw/main/enigma2-softcams-oscam-all-images_11784-emu-801-arm+mips_all.ipk"
 
-wait
+
 opkg install --force-overwrite /tmp/*.ipk
 
 echo ""
@@ -13,7 +13,8 @@ cd ..
 sync
 echo "################ INSTALLATION COMPLETED #############"
 echo "################ 0>>>>>>>>>>>>>100 #################"
-echo "################ RESTARTING... #################" 
+echo "################ RESTARTING... #################"
+echo "################ uploaded by karim #################" 
 init 4
 sleep 2
 init 3
